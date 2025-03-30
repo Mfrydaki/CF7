1d3597a27b226728d994fe52e90459d6441e5904
Maria Frydaki <mfrydaki@gmail.com>
2025-03-30 21:10:12 +0300

    Add files via upload

---
+++ src/gr/aueb/cf/ch26/DateTimeServer.java
@@ -0,0 +1,37 @@
+package gr.aueb.cf.ch26;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class DateTimeServer extends Thread{
+
+    @Override
+    public void run() {
+        ServerSocket servFd;
+        int serverPort = 5000;
+        try {
+            servFd = new ServerSocket();
+            servFd.bind(new InetSocketAddress("localhost", serverPort),100);
+
+            while (true){
+                Socket connectedFd = servFd.accept();
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(connectedFd.getOutputStream()));
+                bw.write("Locaal Server :" + new Date().toString());
+                bw.flush();
+                connectedFd.close();
+            }
+
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+
+
+    }
+}
+
